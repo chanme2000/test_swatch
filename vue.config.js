ffe25f334dadd979af8458be41779c308b6572d3
chanme2000 <chanme2000@naver.com>
2025-08-08 10:51:36 +0900

    change

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   alias : {
-    '@' : FileReader(new URL('./src', import.meta.url))
+    '@' : (new URL('./src', import.meta.url))
   },
   publicPath: '/test_swatch/'
   
