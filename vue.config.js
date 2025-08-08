516b39ecf36f8ffbfd5890ad6578bb3f9e9cf657
chanme2000 <chanme2000@naver.com>
2025-08-08 11:16:21 +0900

    path change alias

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,5 @@
-const { fileURLToPath, URL } = require('node:url');
-const { defineConfig } = require('@vue/cli-service');
-
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  alias : {
-    '@' : fileURLToPath(new URL('./src', import.meta.url))
-  },
   publicPath: '/test_swatch/'
-  
 })
